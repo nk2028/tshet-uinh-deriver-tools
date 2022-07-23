e29b7cab7c4d73dc1970d569ea6c494e754449c6
syimyuzya <syimyuzya@gmail.com>
2022-07-23 13:56:19 +0800

    Remove `選項列表` from constructor parameters

--- src/推導方案.ts
+++ src/推導方案.ts
@@ -1,6 +1,6 @@
 import { 表達式, 適配分析體系, 音韻地位 } from "qieyun";
 
-import type { 原始推導函數, 選項, 選項列表, 選項迭代 } from "./types";
+import type { 原始推導函數, 選項迭代 } from "./types";
 import 推導選項 from "./推導選項";
 
 const 適配poem = 適配分析體系("poem");
@@ -9,31 +9,21 @@ type 推導函數<T> = 推導方案<T>["推導"];
 
 // @ts-expect-error ts(2420)
 export default class 推導方案<T> extends Function implements 推導函數<T> {
-  readonly isLegacy: boolean;
-  readonly parameters: Readonly<選項列表>;
-  readonly defaultOptions: Readonly<選項>;
-  readonly optionsCount: number;
+  readonly 預設配置: 推導選項;
 
-  constructor(
-    private 原始推導函數: 原始推導函數<T>,
-    選項列表: 選項迭代 = (() => {
-      let parameters: 選項迭代;
-      try {
-        parameters = this.原始推導函數();
-        if (typeof parameters[Symbol.iterator] !== "function") return [];
-      } catch {
-        return [];
-      }
-      return parameters;
-    })()
-  ) {
+  constructor(readonly 原始推導函數: 原始推導函數<T>) {
     super();
 
-    const optionInstance = new 推導選項(選項列表);
-    this.isLegacy = optionInstance.isLegacy;
-    this.parameters = optionInstance.parameters;
-    this.defaultOptions = optionInstance.defaultOptions;
-    this.optionsCount = optionInstance.optionsCount;
+    let parameters: 選項迭代 = [];
+    try {
+      const raw = this.原始推導函數();
+      if (typeof parameters[Symbol.iterator] === "function") {
+        parameters = raw;
+      }
+    } finally {
+      // ignore errors in 原始推導函數
+    }
+    this.預設配置 = new 推導選項(parameters);
 
     return new Proxy(this, {
       apply(target, _thisArg, args: Parameters<推導函數<T>>) {
@@ -44,9 +34,10 @@ export default class 推導方案<T> extends Function implements 推導函數<T>
 
   推導(地位: 音韻地位, 字頭: string | null = null, 選項: Record<string, unknown> = {}, ...args: unknown[]) {
     if (!地位) throw new Error("expect 音韻地位");
-    選項 = { ...this.defaultOptions, 選項 };
+    // TODO don't use `預設配置`, obtain a new parameter list & default options instead (same below)
+    選項 = { ...this.預設配置.defaultOptions, 選項 };
     地位 = 適配分析體系.v2extStrict(地位);
-    if (this.isLegacy) {
+    if (this.預設配置.isLegacy) {
       地位 = 適配poem(地位);
       地位.屬於`脣音 或 ${表達式.開合中立韻}` && (地位 = 地位.調整({ 呼: null }));
       地位.屬於`${表達式.重紐母} (${表達式.重紐韻} 或 清韻)` || (地位 = 地位.調整({ 重紐: null }));
