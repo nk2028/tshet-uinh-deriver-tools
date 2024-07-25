99beb672788364b3de03db549601f0fa25b529f1
syimyuzya <syimyuzya@gmail.com>
2024-07-25 23:47:23 +0800

    feat(options): support old parameter list format

--- src/推導設定.test.ts
+++ src/推導設定.test.ts
@@ -1,7 +1,7 @@
 import { 推導設定 } from "./推導設定";
 
 test("建立設定（基本形式）", () => {
-  const 原始設定列表 = [
+  const 設定 = new 推導設定([
     { key: "選項一", default: true },
     { key: "選項二", default: 42 },
     { type: "groupLabel", text: "標籤" },
@@ -9,10 +9,10 @@ test("建立設定（基本形式）", () => {
     {
       key: "選項四",
       default: 0,
-      options: [{ value: "好耶" }, { value: "壞耶", text: "噫（" }],
+      options: [{ value: "好耶" }, { value: "壞耶", text: "噫（" }, { value: 42 }],
     },
-  ] as const;
-  const 設定 = new 推導設定(原始設定列表);
+  ] as const);
+  expect(設定.解析錯誤).toEqual([]);
   expect(設定.列表).toEqual([
     { key: "選項一", default: true },
     { key: "選項二", default: 42 },
@@ -21,7 +21,29 @@ test("建立設定（基本形式）", () => {
     {
       key: "選項四",
       default: "好耶",
-      options: [{ value: "好耶" }, { value: "壞耶", text: "噫（" }],
+      options: [{ value: "好耶" }, { value: "壞耶", text: "噫（" }, { value: 42 }],
+    },
+  ] as const);
+});
+
+test("建立設定（簡略形式）", () => {
+  const 設定 = new 推導設定([
+    ["選項一", true],
+    ["選項二", 42],
+    "標籤",
+    ["選項三|選項名", "orz"],
+    ["選項四", [1, "好耶", { value: "壞耶", text: "噫（" }, 42]],
+  ] as const);
+  expect(設定.解析錯誤).toEqual([]);
+  expect(設定.列表).toEqual([
+    { key: "選項一", default: true },
+    { key: "選項二", default: 42 },
+    { type: "groupLabel", text: "標籤" },
+    { key: "選項三", default: "orz", text: "選項名" },
+    {
+      key: "選項四",
+      default: "好耶",
+      options: [{ value: "好耶" }, { value: "壞耶", text: "噫（" }, { value: 42 }],
     },
   ] as const);
 });
--- src/推導設定.ts
+++ src/推導設定.ts
@@ -25,15 +25,68 @@ export class 推導設定 {
     const 解析錯誤: string[] = [];
     const seenKeys = new Set<string>();
     this.列表 = 設定列表.flatMap((原始設定項, i): 設定項[] => {
-      if (typeof 原始設定項 !== "object") {
-        解析錯誤.push(`error in item #${i}: unrecognized value`);
-        return [];
+      if (isArray(原始設定項)) {
+        if (原始設定項.length === 2 || 原始設定項.length === 3) {
+          const [rawKey, rawDefault, rest = {}] = 原始設定項;
+          if (!(typeof rest === "object" && rest !== null)) {
+            解析錯誤.push(`error in item #${i}: not an object`);
+            return [];
+          }
+          if (typeof rawKey !== "string") {
+            解析錯誤.push(`error in item #${i}: key is not a string`);
+            return [];
+          }
+          const sep = rawKey.indexOf("|");
+          let key = rawKey;
+          let text: string = "";
+          if (sep !== -1) {
+            key = rawKey.slice(0, sep);
+            text = rawKey.slice(sep + 1);
+          }
+
+          let defaultValue = rawDefault;
+          let options = null;
+          if (isArray(rawDefault)) {
+            if (rawDefault.length < 2) {
+              解析錯誤.push(`error in item #${i}: default value is not a valid list`);
+              return [];
+            }
+            defaultValue = rawDefault[0];
+            options = rawDefault.slice(1);
+            if (
+              typeof defaultValue === "number" &&
+              options.findIndex(x => x === defaultValue || (x as { value?: unknown })?.value === defaultValue) === -1
+            ) {
+              defaultValue = defaultValue - 1;
+            }
+          }
+
+          原始設定項 = { ...rest, key, default: defaultValue };
+          if (text) {
+            (原始設定項 as Parameter).text = text;
+          }
+          if (options !== null) {
+            (原始設定項 as Parameter).options = options;
+          }
+        } else {
+          解析錯誤.push(`error in item #${i}: not a valid list`);
+          return [];
+        }
       }
-      if (原始設定項 === null) {
+      if (typeof 原始設定項 === "string") {
+        return [{ type: "groupLabel", text: 原始設定項 }];
+      } else if (typeof 原始設定項 !== "object") {
+        解析錯誤.push(`error in item #${i}: not an object`);
+        return [];
+      } else if (原始設定項 === null) {
         return [{ type: "newline" }];
       } else if ("key" in 原始設定項) {
         if (typeof 原始設定項.key !== "string") {
           解析錯誤.push(`error in item #${i}: 'key' is not a string`);
+          return [];
+        }
+        if (原始設定項.key === "") {
+          解析錯誤.push(`error in item #${i}: empty 'key'`);
           return [];
         }
         if (seenKeys.has(原始設定項.key)) {
@@ -57,8 +110,9 @@ export class 推導設定 {
           }
           const seenValues = new Set();
           const parsedOptions: { value: unknown }[] = [];
-          for (const [j, option] of 設定項.options.entries()) {
-            if (typeof option !== "object" || option === null) {
+          for (const [j, rawOption] of 設定項.options.entries()) {
+            const option: object | null = typeof rawOption === "object" ? rawOption : { value: rawOption };
+            if (option === null) {
               解析錯誤.push(`error in item #${i} option #${j}: unrecognized value`);
               return [];
             } else if (!("value" in option)) {
